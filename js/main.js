b5af6f21dae7d23a3c5ed627d2703d3f6d3401e4
flopeace <florian.tchomga@gmail.com>
2022-11-30 03:32:24 +0100

    fix reponsif and -1 fonction

--- js/main.js
+++ js/main.js
@@ -1,144 +1,172 @@
 // clearing the input on press of clear
 
 let input = $("#input").get(0);
-let number =$(".key_val");
+let number = $(".key_val");
 let operator = $(".operator");
 let result = $("#result").get(0);
-let clear =$("#clear").get(0);
-let response =$("#response").get(0);
+let clear = $("#clear").get(0);
+let response = $("#response").get(0);
 
 resultDisplayed = false; // flag to keep an eye on what output is displayed
 
+// adding click handlers to number buttons
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function (e) {
+    $(".result").removeClass("active");
+    // storing current input string and its last character in letiables - used later
+    let currentString = input.innerHTML;
+    let lastChar = currentString[currentString.length - 1];
 
-
-
+    // if result is not diplayed, just keep adding
+    if (resultDisplayed === false) {
+      input.innerHTML += e.target.innerHTML;
+    } else if (
+      (resultDisplayed === true && lastChar === "+") ||
+      lastChar === "-" ||
+      lastChar === "×" ||
+      lastChar === "÷"
+    ) {
+      // if result is currently displayed and user pressed an operator
+      // we need to keep on adding to the string for next operation
+      resultDisplayed = false;
+      input.innerHTML += e.target.innerHTML;
+    } else {
+      // if result is currently displayed and user pressed a number
+      // we need clear the input string and add the new input to start the new opration
+      resultDisplayed = false;
+      input.innerHTML = "";
+      input.innerHTML += e.target.innerHTML;
+    }
+  });
+}
 
 // adding click handlers to number buttons
-for (var i = 0; i < number.length; i++) {
-    number[i].addEventListener("click", function(e) {
-      $(".result").removeClass("active");
-      // storing current input string and its last character in variables - used later
-      var currentString = input.innerHTML;
-      var lastChar = currentString[currentString.length - 1];
-  
-      // if result is not diplayed, just keep adding
-      if (resultDisplayed === false) {
-        input.innerHTML += e.target.innerHTML;
-      } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-        // if result is currently displayed and user pressed an operator
-        // we need to keep on adding to the string for next operation
-        resultDisplayed = false;
-        input.innerHTML += e.target.innerHTML;
-      } else {
-        // if result is currently displayed and user pressed a number
-        // we need clear the input string and add the new input to start the new opration
-        resultDisplayed = false;
-        input.innerHTML = "";
-        input.innerHTML += e.target.innerHTML;
-      }
-  
-    });
-  }
-  
-  // adding click handlers to number buttons
-  for (var i = 0; i < operator.length; i++) {
-    operator[i].addEventListener("click", function(e) {
-      $(".result").removeClass("active");
-      // storing current input string and its last character in variables - used later
-      var currentString = input.innerHTML;
-      var lastChar = currentString[currentString.length - 1];
-  
-      // if last character entered is an operator, replace it with the currently pressed one
-      if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷" || lastChar === "%") {
-        var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-        input.innerHTML = newString;
-      } else if (currentString.length == 0) {
-        // if first key pressed is an opearator, don't do anything
-        console.log("enter a number first");
-      } else {
-        // else just add the operator pressed to the input
-        input.innerHTML += e.target.innerHTML;
-      }
-  
-    });
-  }
-  
-  // on click of 'equal' button
-result.addEventListener("click", function() {
-  
-    // this is the string that we will be processing eg. -10+26+33-56*34/23
-    var inputString = input.innerHTML;
-  
-    // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-    var numbers = inputString.split(/\+|\%|\-|\×|\÷/g);
-  
-    // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-    // first we replace all the numbers and dot with empty string and then split
-    var operators = inputString.replace(/[0-9]|\./g, "").split("");
-  
-    console.log(inputString);
-    console.log(operators);
-    console.log(numbers);
-    console.log("----------------------------");
-  
-    // now we are looping through the array and doing one operation at a time.
-    // first divide, then multiply, then subtraction and then addition
-    // as we move we are alterning the original numbers and operators array
-    // the final element remaining in the array will be the output
-  
-    var divide = operators.indexOf("÷");
-    while (divide != -1) {
-      numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-      operators.splice(divide, 1);
-      divide = operators.indexOf("÷");
-    }
-  
-    var multiply = operators.indexOf("×");
-    while (multiply != -1) {
-      numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-      operators.splice(multiply, 1);
-      multiply = operators.indexOf("×");
-    }
-  
-    var subtract = operators.indexOf("-");
-    while (subtract != -1) {
-      numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-      operators.splice(subtract, 1);
-      subtract = operators.indexOf("-");
-    }
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function (e) {
+    $(".result").removeClass("active");
+    // storing current input string and its last character in letiables - used later
+    let currentString = input.innerHTML;
+    let lastChar = currentString[currentString.length - 1];
+    let firstChar = e.target.innerHTML;
 
-    var modulo = operators.indexOf("%");
-    while (modulo != -1) {
-      numbers.splice(modulo, 2, numbers[modulo] % numbers[modulo + 1]);
-      operators.splice(modulo, 1);
-      modulo = operators.indexOf("-");
+    console.log( currentString[0]);
+    // if last character entered is an operator, replace it with the currently pressed one
+    if(firstChar[0]==="-"){
+      console.log("negation calculation");
+      let newString =
+      currentString.substring(0, currentString.length - 1) +
+      e.target.innerHTML;
+      input.innerHTML = newString;
     }
-  
-    var add = operators.indexOf("+");
-    while (add != -1) {
-      // using parseFloat is necessary, otherwise it will result in string concatenation :)
-      numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-      operators.splice(add, 1);
-      add = operators.indexOf("+");
+   
+    if (
+      lastChar === "+" ||
+      lastChar === "-" ||
+      lastChar === "×" ||
+      lastChar === "÷" ||
+      lastChar === "%"
+    ) {
+      let newString =
+        currentString.substring(0, currentString.length - 1) +
+        e.target.innerHTML;
+      input.innerHTML = newString;
+    } else if (currentString.length == 0) {
+      // if first key pressed is an opearator, don't do anything
+      console.log("enter a number first");
+
+
+
+
+    } else {
+      // else just add the operator pressed to the input
+      input.innerHTML += e.target.innerHTML;
     }
-  
-    // input.innerHTML = numbers[0]; // displaying the output
-  
-    response.innerHTML = numbers[0]; // displaying the output
-    $(".result").addClass("active");
-  
-    resultDisplayed = true; // turning flag if result is displayed
   });
-  
-  // clearing the input on press of clear
-  clear.addEventListener("click", function() {
-    input.innerHTML = "";
-    $(".result").removeClass("active");
-  })
-  // delete the input on press of delete
-  let deleted =$(".delete").get(0);
-  deleted.addEventListener("click", function() {
-    let delete_string =input.innerHTML;
-    input.innerHTML =delete_string.slice(0,-1);
-    $(".result").removeClass("active");
-  })
+}
+
+// on click of 'equal' button
+result.addEventListener("click", function () {
+  // this is the string that we will be processing eg. -10+26+33-56*34/23
+  let inputString = input.innerHTML;
+
+  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+  let numbers = inputString.split(/\+|\%|\-|\×|\÷/g);
+
+  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+  // first we replace all the numbers and dot with empty string and then split
+  let operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+  console.log(inputString);
+  console.log(operators);
+  console.log(numbers);
+  console.log("----------------------------");
+
+  // now we are looping through the array and doing one operation at a time.
+  // first divide, then multiply, then subtraction and then addition
+  // as we move we are alterning the original numbers and operators array
+  // the final element remaining in the array will be the output
+
+  let divide = operators.indexOf("÷");
+  while (divide != -1) {
+    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+    operators.splice(divide, 1);
+    divide = operators.indexOf("÷");
+  }
+
+  let multiply = operators.indexOf("×");
+  while (multiply != -1) {
+    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+    operators.splice(multiply, 1);
+    multiply = operators.indexOf("×");
+  }
+
+  let subtract = operators.indexOf("-");
+  while (subtract != -1) {
+    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+    operators.splice(subtract, 1);
+    subtract = operators.indexOf("-");
+  }
+
+  let modulo = operators.indexOf("%");
+  while (modulo != -1) {
+    numbers.splice(modulo, 2, numbers[modulo] % numbers[modulo + 1]);
+    operators.splice(modulo, 1);
+    modulo = operators.indexOf("-");
+  }
+
+  let add = operators.indexOf("+");
+  while (add != -1) {
+    // using parseFloat is necessary, otherwise it will result in string concatenation :)
+    numbers.splice(
+      add,
+      2,
+      parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
+    );
+    operators.splice(add, 1);
+    add = operators.indexOf("+");
+  }
+
+  // input.innerHTML = numbers[0]; // displaying the output
+
+  response.innerHTML = numbers[0]; // displaying the output
+  $(".result").addClass("active");
+
+  resultDisplayed = true; // turning flag if result is displayed
+});
+
+// clearing the input on press of clear
+clear.addEventListener("click", function () {
+  input.innerHTML = "";
+  $(".result").removeClass("active");
+});
+// delete the input on press of delete
+let deleted = $(".delete").get(0);
+deleted.addEventListener("click", function () {
+  let delete_string = input.innerHTML;
+  input.innerHTML = delete_string.slice(0, -1);
+  $(".result").removeClass("active");
+});
+
+$(".key").click(function (param) {
+  navigator.vibrate(12);
+});
